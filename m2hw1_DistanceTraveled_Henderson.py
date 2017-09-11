2d4f3695e5797180ba61eb83e9e8d9a14f6b163b
hendersj5018 <31700892+hendersj5018@users.noreply.github.com>
2017-09-11 08:19:27 -0400

    Add files via upload

---
+++ m2hw1_DistanceTraveled_Henderson.py
@@ -0,0 +1,25 @@
+#CTI_110
+#H2HW1 - Distance Traveled
+#Jasper Henderson
+#10Sep2017
+#
+
+#Distance=speed*time
+
+speed=70
+
+time=6
+distance=speed*time
+
+print('the distance after 6hours is', distance)
+
+time=10
+
+distance=speed*10
+
+print('the distance after 10hours is', distance)
+
+time=15
+print('the distance after 15hours is', distance)
+'input( distance=speed*time)'
+
